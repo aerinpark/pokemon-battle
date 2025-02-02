43e42dec7d7305d8b55e33c36ac3bd75d6ef27df
aerinp <78767681+aerinpark@users.noreply.github.com>
2025-02-02 14:18:57 -0800

    Add playRound function to play a round of game
    playRound takes two arguments and perform a rock paper scissors and increments either a human or computer score accordingly.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const CHOICES = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     return CHOICES[Math.floor(Math.random()*3)];
@@ -13,3 +15,39 @@ function getHumanChoice(){
         humanChoice = prompt("Invalid input. Rock, Paper or Scissors?");
     }
 }
+
+function playRound(computer, human){
+    if (computer == human){
+        return "It's a tie!";
+    } else if (computer == "rock"){
+        if (human == "paper"){
+            humanScore++;
+            return "You Win! Paper beats Rock.";
+        }else {
+            computerScore++;
+            return "You Lose! Rock beats Scissors.";
+        }
+    } else if (computer == "paper"){
+        if (human == "rock"){
+            computerScore++;
+            return "You Lose! Paper beats Rock.";
+        }else {
+            humanScore++;
+            return "You Win! Scissors beats Paper!";
+        }
+    } else {
+        if (human == "rock"){
+            humanScore++;
+            return "You Win! Rock beats Scissors.";
+        }else {
+            computerScore++;
+            return "You Lose! Scissors beats Paper.";
+        }
+    }
+}
+
+const compt = getComputerChoice();
+const human = getHumanChoice();
+console.log(playRound(compt, human));
+console.log(`comp: ${computerScore}, human: ${humanScore}`);
+
