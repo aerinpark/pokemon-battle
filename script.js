ef7315cf1171826faa2debce0ef654ef005e9de4
aerinp <78767681+aerinpark@users.noreply.github.com>
2025-02-02 14:06:08 -0800

    Add getHumanChoice function
    Prompt user for their choice, and any invalid option will re-prompt the user for the proper option.

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-CHOICES = ["rock", "paper", "scissors"];
+const CHOICES = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
     return CHOICES[Math.floor(Math.random()*3)];
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(){
+    let humanChoice = (prompt("Rock, Paper or Scissors?")).toLowerCase();
+    while(true){
+        if (CHOICES.includes(humanChoice)){
+            return humanChoice;
+        }
+        humanChoice = prompt("Invalid input. Rock, Paper or Scissors?");
+    }
+}
