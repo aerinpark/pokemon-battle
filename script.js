a2cea71782a90550889c63fb4cfe8489d6b73706
aerinp <78767681+aerinpark@users.noreply.github.com>
2025-02-07 21:27:13 -0800

    add result generation

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const CHOICES = ["rock", "paper", "scissors"];
+const PLAYER_POKEMON = ["charmander", "squirtle", "bulbasaur"];
+const COMPUTER_POKEMON = ["litten", "popplio", "rowlet"];
+const TYPES = ["fire", "water", "grass"];
+const FIRE = "Fire beats Grass.";
+const WATER = "Water beats Fire.";
+const GRASS = "Grass beats Water.";
+
 let humanScore = 0;
 let computerScore = 0;
 let gameOn = true;
@@ -7,68 +13,42 @@ const buttons = document.querySelectorAll("button");
 const score = document.querySelector("#score");
 const againBtn = document.createElement("button");
 const scoreSection = document.querySelector(".score-sec");
+const computerPokemon = document.querySelector("#opp-pokemon");
+const playerPokemon = document.querySelector("#player-pokemon");
+const gameMessage = document.querySelector(".msg");
 
 function getComputerChoice() {
-	return CHOICES[Math.floor(Math.random() * 3)];
-}
-
-function getHumanChoice() {
-	let humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
-	while (true) {
-		if (CHOICES.includes(humanChoice)) {
-			return humanChoice;
-		}
-		humanChoice = prompt("Invalid input. Rock, Paper or Scissors?");
-	}
-}
-
-function updateScore() {
-	score.textContent = `${computerScore} : ${humanScore}`;
+	return TYPES[Math.floor(Math.random() * 3)];
 }
 
 function playRound(computer, human) {
+	updatePokemon(computer, human);
 	if (computer == human) {
-		console.log("It's a tie!");
-	} else if (computer == "rock") {
-		if (human == "paper") {
+        gameMessage.textContent = "It's a tie!";
+	} else if (computer == "fire") {
+		if (human == "water") {
 			humanScore++;
-			console.log("You Win! Paper beats Rock.");
+            gameMessage.textContent = `You Win! ${WATER}`;
 		} else {
 			computerScore++;
-			console.log("You Lose! Rock beats Scissors.");
+            gameMessage.textContent = `You Lose! ${FIRE}`;
 		}
-	} else if (computer == "paper") {
-		if (human == "rock") {
+	} else if (computer == "water") {
+		if (human == "fire") {
 			computerScore++;
-			console.log("You Lose! Paper beats Rock.");
+            gameMessage.textContent = `You Lose! ${WATER}`;
 		} else {
 			humanScore++;
-			console.log("You Win! Scissors beats Paper.");
+            gameMessage.textContent = `You Win! ${GRASS}`;
 		}
 	} else {
-		if (human == "rock") {
+		if (human == "fire") {
 			humanScore++;
-			console.log("You Win! Rock beats Scissors.");
+            gameMessage.textContent = `You Win! ${FIRE}`;
 		} else {
 			computerScore++;
-			console.log("You Lose! Scissors beats Paper.");
+            gameMessage.textContent = `You Lose! ${GRASS}`;
 		}
-	}
-	updateScore();
-	if (humanScore >= 5 || computerScore >= 5) {
-		gameOn = false;
-		if (computerScore >= 5) {
-			score.textContent = "You Lose!";
-		} else {
-			score.textContent = "You Win!";
-		}
-		againBtn.textContent = "Again?";
-		scoreSection.setAttribute(
-			"style",
-			"display: flex; gap: 20px; align-items: center;"
-		);
-		scoreSection.style.display = "flex";
-		scoreSection.appendChild(againBtn);
 	}
 }
 
@@ -77,7 +57,20 @@ function resetGame() {
 	scoreSection.removeChild(againBtn);
 	humanScore = 0;
 	computerScore = 0;
-	score.textContent = "0 : 0";
+}
+
+function updatePokemon(computer, human) {
+	computerPokemon.src = `./images/${COMPUTER_POKEMON[TYPES.indexOf(computer)]}.png`;
+	computerPokemon.setAttribute("style", "height: 40%; width: auto; align-self: flex-start; transform: scaleX(-1);");
+
+	TYPES.forEach((type) => {
+		if (type == human) {
+			playerPokemon.src = `./images/${
+				PLAYER_POKEMON[TYPES.indexOf(type)]
+			}.png`;
+			playerPokemon.setAttribute("style", "height: 40%; width: auto; align-self: flex-end;");
+		}
+	});
 }
 
 // The game is initiated when any of the buttons is clicked.
